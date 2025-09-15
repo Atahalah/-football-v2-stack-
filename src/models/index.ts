455e71607e1b85e57ab8ad39703af50881a3a107
Atahalah <atahakini@gmail.com>
2025-09-15 06:24:09 +0200

    Added package.json

--- src/models/index.ts
+++
@@ -1,7 +0,0 @@
-export { BaseModel } from './BaseModel';
-export { PoissonModel } from './PoissonModel';
-export { EloModel } from './EloModel';
-export { MachineLearningModel } from './MachineLearningModel';
-export { EnsembleModel } from './EnsembleModel';
-export { TacticalModel } from './TacticalModel';
-export { StrategicModel } from './StrategicModel';
